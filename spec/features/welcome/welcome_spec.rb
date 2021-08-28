e2c015d6a867a1d487549d7e6ebe6aba28ab66ad
Erika Kischuk <erika.kischuk@gmail.com>
2021-08-28 17:03:27 -0600

    Add feature test for welcome controller action welcome view and links

---
+++ spec/features/welcome/welcome_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'welcome page' do
+  it 'has welcome page' do
+    visit '/'
+
+    expect(page).to have_content('Memberships')
+    expect(page).to have_content('Choirs')
+    expect(page).to have_content('Climbing Gyms')
+  end
+
+  it 'has parent index links' do
+    visit '/'
+    click_on 'Choirs'
+    expect(current_path).to eq('/choirs')
+
+    visit '/'
+    click_on 'Climbing Gyms'
+    expect(current_path).to eq('/climbing_gyms')
+  end
+end
