eda3abdc5888d27fa826550f311b075fdb9a6220
Erika Kischuk <erika.kischuk@gmail.com>
2021-08-29 13:16:31 -0600

    Add test for member count in show action test

--- spec/features/climbing_gyms/show_spec.rb
+++ spec/features/climbing_gyms/show_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'climbing gym show page' do
   before(:each) do
-    @et = ClimbingGym.create!(name: "EarthTreks", ropes: true, total_routes: 200)
-    @movement = ClimbingGym.create!(name: "Movement RiNo", ropes: false, total_routes: 70)
+    @et = ClimbingGym.create!(name: "EarthTreks", ropes: true, total_routes: 200, updated_at: '2021-08-26 01:11:07 UTC', created_at: '2021-08-26 01:11:07 UTC')
+    @movement = ClimbingGym.create!(name: "Movement RiNo", ropes: false, total_routes: 70, updated_at: '2021-08-27 01:11:07 UTC', created_at: '2021-08-27 01:11:07 UTC')
+    @dbc = ClimbingGym.create!(name: "Denver Bouldering Club", ropes: false, total_routes: 85, updated_at: '2021-08-26 01:12:07 UTC', created_at: '2021-08-26 02:11:07 UTC')
+    @amy = GymMember.create!(climbing_gym: @et, first_name: "Amy", last_name: "Santiago", belay_status: true, monthly_checkins: 12)
+    @jake = GymMember.create!(climbing_gym: @et, first_name: "Jake", last_name: "Peralta", belay_status: false, monthly_checkins: 3)
+    @rosa = GymMember.create!(climbing_gym: @movement, first_name: "Rosa", last_name: "Diaz", belay_status: true, monthly_checkins: 14)
   end
-  
+
   it 'displays the gym name' do
     visit "/climbing_gyms/#{@et.id}"
 
@@ -22,5 +26,16 @@ RSpec.describe 'climbing gym show page' do
     visit "/climbing_gyms/#{@et.id}"
 
     expect(page).to have_content("Number of Routes: 200")
+  end
+# As a visitor
+# When I visit a parent's show page
+# I see a count of the number of children associated with this parent
+
+  it 'displays number of members associated' do
+    visit "/climbing_gyms/#{@et.id}"
+    expect(page).to have_content("Total Members: 2")
+
+    visit "/climbing_gyms/#{@dbc.id}"
+    expect(page).to have_content("Total Members: 0")
   end
 end
