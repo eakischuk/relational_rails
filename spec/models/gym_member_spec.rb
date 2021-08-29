74e47a46e8bef708ef0c09065dd2a8ff470ffd6e
Erika Kischuk <erika.kischuk@gmail.com>
2021-08-28 18:38:09 -0600

    Add full_name method to gym_member model with test

--- spec/models/gym_member_spec.rb
+++ spec/models/gym_member_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe GymMember do
   it {should belong_to :climbing_gym}
+
+  it 'has full name' do
+    @et = ClimbingGym.create!(name: "EarthTreks", ropes: true, total_routes: 200)
+    @amy = GymMember.create!(climbing_gym: @et, first_name: "Amy", last_name: "Santiago", belay_status: true, monthly_checkins: 12)
+
+    expect(@amy.full_name).to eq("Amy Santiago")
+  end
 end
