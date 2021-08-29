4c09e10c7d1f8ae5c6a818ed18dcde3bbc0f2bfa
Erika Kischuk <erika.kischuk@gmail.com>
2021-08-29 17:32:35 -0600

    Add new action to climbing gym controller with routes and form in new view

--- app/controllers/climbing_gyms_controller.rb
+++ app/controllers/climbing_gyms_controller.rb
@@ -6,4 +6,17 @@ class ClimbingGymsController < ApplicationController
   def show
     @gym = ClimbingGym.find(params[:gym_id])
   end
+
+  def new
+  end
+
+  def create
+    climbing_gym = ClimbingGym.new({
+      name: params[:name],
+      ropes: params[:ropes_available],
+      total_routes: params[:total_routes]
+    })
+    climbing_gym.save
+    redirect_to '/climbing_gyms'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#welcome'
   get '/choirs', to: 'choirs#index'
   get '/climbing_gyms', to: 'climbing_gyms#index'
+  get '/climbing_gyms/new', to: 'climbing_gyms#new'
+  post '/climbing_gyms', to: 'climbing_gyms#create'
   get '/climbing_gyms/:gym_id', to: 'climbing_gyms#show'
+  get '/climbing_gyms/:gym_id/gym_members', to: 'climbing_gym_gym_members#index'
   get '/gym_members', to: 'gym_members#index'
   get '/gym_members/:member_id', to: 'gym_members#show'
-  get '/climbing_gyms/:gym_id/gym_members', to: 'climbing_gym_gym_members#index'
 end
