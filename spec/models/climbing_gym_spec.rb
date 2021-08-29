817b2324d95746e511605ef5cc68a6efd45ee606
Erika Kischuk <erika.kischuk@gmail.com>
2021-08-29 13:08:55 -0600

    Add test for member count instance method

--- spec/models/climbing_gym_spec.rb
+++ spec/models/climbing_gym_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe ClimbingGym, type: :model do
     @et = ClimbingGym.create!(name: "EarthTreks", ropes: true, total_routes: 200, updated_at: '2021-08-26 01:11:07 UTC', created_at: '2021-08-26 01:11:07 UTC')
     @movement = ClimbingGym.create!(name: "Movement RiNo", ropes: false, total_routes: 70, updated_at: '2021-08-27 01:11:07 UTC', created_at: '2021-08-27 01:11:07 UTC')
     @dbc = ClimbingGym.create!(name: "Denver Bouldering Club", ropes: false, total_routes: 85, updated_at: '2021-08-26 01:12:07 UTC', created_at: '2021-08-26 02:11:07 UTC')
+    @amy = GymMember.create!(climbing_gym: @et, first_name: "Amy", last_name: "Santiago", belay_status: true, monthly_checkins: 12)
+    @jake = GymMember.create!(climbing_gym: @et, first_name: "Jake", last_name: "Peralta", belay_status: false, monthly_checkins: 3)
+    @rosa = GymMember.create!(climbing_gym: @movement, first_name: "Rosa", last_name: "Diaz", belay_status: true, monthly_checkins: 14)
   end
   it {should have_many :gym_members}
 
@@ -12,6 +15,14 @@ RSpec.describe ClimbingGym, type: :model do
     it 'orders by creation date' do
       gyms = ClimbingGym.order_by_creation
       expect(gyms).to eq([@movement, @dbc, @et])
+    end
+  end
+
+  describe 'instance methods' do
+    it 'counts members' do
+      expect(@et.member_count).to eq(2)
+      expect(@movement.member_count).to eq(1)
+      expect(@dbc.member_count).to eq(0)
     end
   end
 end
