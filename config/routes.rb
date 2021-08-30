da1b934ef55531636909393503c7548b87a0710f
Erika Kischuk <erika.kischuk@gmail.com>
2021-08-29 18:32:28 -0600

    Add edit and update actions to climbing gyms with routes and edit view

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get '/climbing_gyms/new', to: 'climbing_gyms#new'
   post '/climbing_gyms', to: 'climbing_gyms#create'
   get '/climbing_gyms/:gym_id', to: 'climbing_gyms#show'
+  get '/climbing_gyms/:gym_id/edit', to: 'climbing_gyms#edit'
+  patch '/climbing_gyms/:gym_id', to: 'climbing_gyms#update'
   get '/climbing_gyms/:gym_id/gym_members', to: 'climbing_gym_gym_members#index'
   get '/gym_members', to: 'gym_members#index'
   get '/gym_members/:member_id', to: 'gym_members#show'
