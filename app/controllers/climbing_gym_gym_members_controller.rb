4eac6b893955c38a92beb018bdcaf6daad72c855
Erika Kischuk <erika.kischuk@gmail.com>
2021-08-29 20:22:26 -0600

    Add new action to climbing gym gym members controller with route view and functionality

--- app/controllers/climbing_gym_gym_members_controller.rb
+++ app/controllers/climbing_gym_gym_members_controller.rb
@@ -3,4 +3,20 @@ class ClimbingGymGymMembersController < ApplicationController
     @gym = ClimbingGym.find(params[:gym_id])
     @members = @gym.gym_members
   end
+
+  def new
+    @gym = ClimbingGym.find(params[:gym_id])
+  end
+
+  def create
+    gym_member = GymMember.new({
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      belay_status: params[:belay_status],
+      monthly_checkins: params[:checkins],
+      climbing_gym_id: params[:gym_id]
+      })
+    gym_member.save
+    redirect_to "/climbing_gyms/#{params[:gym_id]}/gym_members"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get '/climbing_gyms/:gym_id/edit', to: 'climbing_gyms#edit'
   patch '/climbing_gyms/:gym_id', to: 'climbing_gyms#update'
   get '/climbing_gyms/:gym_id/gym_members', to: 'climbing_gym_gym_members#index'
+  get '/climbing_gyms/:gym_id/gym_members/new', to: 'climbing_gym_gym_members#new'
+  post '/climbing_gyms/:gym_id/gym_members', to: 'climbing_gym_gym_members#create'
   get '/gym_members', to: 'gym_members#index'
   get '/gym_members/:member_id', to: 'gym_members#show'
 end
