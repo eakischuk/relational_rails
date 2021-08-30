1a8d16afffb1d3246ccb9dcd0ec6af65b98fa2d7
Miriam Aceves <miriammerari02@gmail.com>
2021-08-29 23:02:16 -0400

    Rearrange lines

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#welcome'
+  
   get '/choirs', to: 'choirs#index'
   get '/choirs/:id', to: 'choirs#show'
-
-  get '/choirs/:choir_id/choir_members', to: 'choirs_choir_members#index'
-
   get '/choir_members', to: 'choir_members#index'
   get '/choir_members/:id', to: 'choir_members#show'
+  get '/choirs/:choir_id/choir_members', to: 'choirs_choir_members#index'
 
   get '/climbing_gyms', to: 'climbing_gyms#index'
   get '/climbing_gyms/:gym_id', to: 'climbing_gyms#show'
