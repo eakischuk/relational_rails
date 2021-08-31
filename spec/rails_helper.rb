19cc5f2c3633143ac09b22f1425423406982bc26
Miriam Aceves <miriammerari02@gmail.com>
2021-08-30 21:26:47 -0400

    Remove merge message

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,10 +65,9 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
-<<<<<<< HEAD
 
-=======
->>>>>>> 10237bb2c59c73c8f61afa51127c7bdf4343a534
+
+
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
