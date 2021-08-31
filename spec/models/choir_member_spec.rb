de87dbc510e674510004c4a1d014aec05c428cc2
Miriam Aceves <miriammerari02@gmail.com>
2021-08-30 21:28:01 -0400

    Create tests for choir member model

--- spec/models/choir_member_spec.rb
+++ spec/models/choir_member_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe ChoirMember, type: :model do
-  it {should belong_to :choir}
+
+  describe "Relationships" do
+    it {should belong_to :choir}
+
+  end
+
+  describe "Validations" do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :age}
+    it {should validate_presence_of :active}
+    it {should validate_presence_of :voice_type}
+  end
 end
