ed4b9d3863c1ac9b4c7efc3e0d5bd0202d313dc4
Erika Kischuk <erika.kischuk@gmail.com>
2021-08-31 20:28:31 -0600

    Add test for min checkins class method

--- spec/models/gym_member_spec.rb
+++ spec/models/gym_member_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe GymMember, type: :model do
+  before(:each) do
+    @et = ClimbingGym.create!(name: "EarthTreks", ropes: true, total_routes: 200)
+    @jake = GymMember.create!(climbing_gym: @et, first_name: "Jake", last_name: "Peralta", belay_status: false, monthly_checkins: 3)
+    @amy = GymMember.create!(climbing_gym: @et, first_name: "Amy", last_name: "Santiago", belay_status: true, monthly_checkins: 12)
+    @rosa = GymMember.create!(climbing_gym: @et, first_name: "Rosa", last_name: "Diaz", belay_status: true, monthly_checkins: 14)
+  end
+
   it {should belong_to :climbing_gym}
 
-  it 'has full name' do
-    @et = ClimbingGym.create!(name: "EarthTreks", ropes: true, total_routes: 200)
-    @amy = GymMember.create!(climbing_gym: @et, first_name: "Amy", last_name: "Santiago", belay_status: true, monthly_checkins: 12)
+  describe 'instance methods' do
+    it 'has full name' do
 
-    expect(@amy.full_name).to eq("Amy Santiago")
+      expect(@amy.full_name).to eq("Amy Santiago")
+    end
+  end
+
+  describe 'class methods' do
+    it 'filters by number of checkins' do
+      expect(GymMember.checkin_min).to eq([@jake, @amy, @rosa])
+      expect(GymMember.checkin_min(5)).to eq([@amy, @rosa])
+    end
   end
 end
