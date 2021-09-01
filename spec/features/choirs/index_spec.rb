aff64113760ed2e0066eb98af49f2e79bd387ce7
Miriam Aceves <miriammerari02@gmail.com>
2021-09-01 19:40:42 -0400

    Update tests to run successfully

--- spec/features/choirs/index_spec.rb
+++ spec/features/choirs/index_spec.rb
@@ -18,7 +18,32 @@ RSpec.describe 'the choirs home page', type: :feature do
 
     expect(page).to have_content("Divo - Date Created: #{@divo.created_at}")
     expect(page).to have_content("Volcestra - Date Created: #{@volcestra.created_at}")
-    expect("#{@divo.name}").to appear_before("#{@volcestra.created_at}", only_text: true)
-    expect("Divo - Date Created: #{@divo.created_at}").to appear_before("Volcestra - Date Created: #{@volcestra.created_at}", only_text: true)
+    expect("Volcestra - Date Created: #{@volcestra.created_at}").to appear_before("Divo - Date Created: #{@divo.created_at}", only_text: true)
+  end
+
+  it 'shows creation date' do
+    visit "/choirs"
+
+    expect(page).to have_content("Volcestra - Date Created: #{@volcestra.created_at}")
+    expect(page).to have_content("Divo - Date Created: #{@divo.created_at}")
+    expect("#{@volcestra.name}").to appear_before("#{@divo.created_at}", only_text: true)
+    expect("Volcestra - Date Created: #{@volcestra.created_at}").to appear_before("Divo - Date Created: #{@divo.created_at}", only_text: true)
+  end
+
+  it 'has all members link' do
+    visit "/choirs"
+
+    click_on 'All Choir Members'
+    expect(current_path).to eq('/choir_members')
+  end
+
+  it 'has update buttons for each choir' do
+    visit "/choirs"
+    click_on 'Update Volcestra'
+    expect(current_path).to eq("/choirs/#{@volcestra.id}/edit")
+
+    visit "/choirs"
+    click_on 'Update Divo'
+    expect(current_path).to eq("/choirs/#{@divo.id}/edit")
   end
 end
